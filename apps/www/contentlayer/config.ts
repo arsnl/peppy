469c6b25043450394a90f939930d42f72ca581e0
Mark Arseneault <mark@arsnl.dev>
2023-08-29 12:14:55 -0400

    chore: migrate versions to contentlayer

--- apps/www/contentlayer/config.ts
+++ apps/www/contentlayer/config.ts
@@ -2,10 +2,11 @@ import { makeSource } from "contentlayer/source-files";
 import { Doc } from "./document-types/doc";
 import { ESLintConfig } from "./document-types/eslint-config";
 import { Rule } from "./document-types/rule";
+import { Version } from "./document-types/version";
 import { mdxOptions } from "./mdx-options";
 
 export const contentLayerConfig = makeSource({
   contentDirPath: "./src/content",
-  documentTypes: [Doc, Rule, ESLintConfig],
+  documentTypes: [Doc, Rule, ESLintConfig, Version],
   mdx: mdxOptions,
 });
